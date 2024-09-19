690c74a8b66cbf6614245ba3c2e22ff338e70e32
Ryan Maxwell <ryan@ryanjmaxwell.ca>
2024-09-19 18:33:22 -0400

    added logging to route file

--- app/api/sendEmail/route.js
+++ app/api/sendEmail/route.js
@@ -11,7 +11,7 @@ export async function POST(req) {
       secure: false, 
       auth: {
         user: 'AKIAU4U3VUHSXLLZSXWO',
-        pass: 'BH6UY8EBuH5myvBmHj3Ybbgrm/mL+1YWZQP5aEURUnx0'//process.env.EMAIL_PASS,  Ensure EMAIL_PASS is stored as an env variable
+        pass: 'BH6UY8EBuH5myvBmHj3Ybbgrm/mL+1YWZQP5aEURUnx0'
       },
     });
 
@@ -23,15 +23,15 @@ export async function POST(req) {
     };
 
     // Log transporter configuration for debugging
-/*     console.log('Transporter configuration:', transporter.options);
- */
+     console.log('Transporter configuration:', transporter.options);
+ 
     const info = await transporter.sendMail(mailOptions);
     /* console.log('Email sent successfully:', info.response); */
 
     return new Response(JSON.stringify({ success: true }), { status: 200 });
   } catch (error) {
     // Log the full error for debugging
-/*     console.error('Error sending email:', error);
- */    return new Response(JSON.stringify({ success: false, error: error.message }), { status: 500 });
+     console.error('Error sending email:', error);
+     return new Response(JSON.stringify({ success: false, error: error.message }), { status: 500 });
   }
 }
